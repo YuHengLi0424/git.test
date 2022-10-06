e7d502d2877365b353bf50fb209ca5efe50c902e
xenon <768527756@qq.com>
2022-10-06 20:43:39 +0800

    hot-fix commit

--- src/main/java/com/git/gitTest.java
+++ src/main/java/com/git/gitTest.java
@@ -3,7 +3,7 @@ package com.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println( "hello");
-        System.out.println("hello git");
+        System.out.println("hello git1");
         System.out.println("hello git");
     }
 }
